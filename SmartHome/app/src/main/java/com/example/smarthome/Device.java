79f736b226978b14c7dadb71fd8e7eb4851be33c
lkarpovich <lkarpovich@hotmail.com>
2019-11-16 16:16:11 -0300

    just rooms

---
+++ SmartHome/app/src/main/java/com/example/smarthome/Device.java
@@ -0,0 +1,65 @@
+package com.example.smarthome;
+
+import android.content.Context;
+
+import java.io.Serializable;
+
+public class Device implements Serializable {
+    private String id;
+    private String name;
+    private String typeId;
+    private String meta; //will be empty
+
+
+
+    public Device(String name, String typeId, String meta){
+        this.name = name;
+        this.typeId = typeId;
+        this.meta = meta;
+    }
+
+//    public void onClickAction(Serializable arg, final Context context) {
+//
+//    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTypeId() { return typeId; }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMeta(String meta) {
+        this.meta = meta;
+    }
+
+    public String getMeta() {
+        return this.meta;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + this.id + "; typeId: "+ this.typeId + "; name: "+ this.name+"; meta: "+ this.meta;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o == null || !(o instanceof Device)) {
+            return false;
+        }
+
+        Device d = (Device)o;
+        return this.name.equals(d.name) && this.typeId.equals(d.typeId) && this.meta.equals(d.meta);
+    }
+}
