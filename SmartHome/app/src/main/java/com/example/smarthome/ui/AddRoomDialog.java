d75fc9fdd3c1e6b3eec73cbfc14875d24247813b
mtarradellas <34386411+mtarradellas@users.noreply.github.com>
2019-11-22 01:22:26 -0300

    add room and room list done

--- SmartHome/app/src/main/java/com/example/smarthome/ui/AddRoomDialog.java
+++ SmartHome/app/src/main/java/com/example/smarthome/ui/AddRoomDialog.java
@@ -42,6 +42,7 @@ public class AddRoomDialog extends AppCompatDialogFragment {
                     public void onClick(DialogInterface dialog, int which) {
                         String name = inputEditText.getText().toString();
                         addRoomListener.applyAddRoom(name);
+
                     }
                 });
         return builder.create();
